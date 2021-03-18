6553bf69fe53a25fec363ec15359f4e979182249
andraderegis <reginaldo.andrade@outlook.com>
2021-03-18 20:35:03 -0300

    Add Routes, pages SignIn and SignOut.

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignIn = (): JSX.Element => {
+  return <Container />;
+};
+
+export default SignIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignUp = (): JSX.Element => {
+  return <Container />;
+};
+
+export default SignUp;
